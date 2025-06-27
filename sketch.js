005c315a4e771dd28c23c1efe50aef975d8278c6
Mau! <mau.giachero@gmail.com>
2025-06-27 13:52:00 -0300

    first steps

--- sketch.js
+++ sketch.js
@@ -1,14 +1,26 @@
+/**
+ * faceMesh variables
+ */
 let faceMesh;
 let options = { maxFaces: 1, refineLandmarks: false, flipped: false };
+
+/**
+ * variables
+ */
+let video;
 
 function preload() {
   faceMesh = ml5.faceMesh(options);
 }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 500, WEBGL);
+  video = createCapture(VIDEO);
 }
 
 function draw() {
+  translate(-(width / 2), -(height / 2));
   background(220);
+  fill(255, 0, 0);
+  circle(0, 0, 20);
 }
