7748617fe18937c73a2e32071c8f7f1b2ce9d371
Mauro Giachero <mau.giachero@gmail.com>
2025-06-28 15:06:56 -0300

    agregado de readme y human detected phase

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Agente
+
+## Features para agregar
+
+- Scanner en la cara que diga algo como "cargando datos" u "obteniendo datos"
+- Tal vez hacer una captura de pantalla mover a un lado la cámara del interactor y empezar a escribir los datos
+- Obtener datos de API para dibujar, o bajar datos random y ordenarlos en un json para mostrarlos en la obra
--- sketch.js
+++ sketch.js
@@ -1,14 +1,20 @@
 /**
  * faceMesh variables
  */
+
 let faceMesh;
 let options = { maxFaces: 1, refineLandmarks: false, flipped: false };
-let face = [];
+let faces = [];
+let face;
+let faceDetected = 0;
 
 /**
  * variables
  */
+
 let video;
+2;
+let ms;
 
 function preload() {
   faceMesh = ml5.faceMesh(options);
@@ -20,13 +26,39 @@ function setup() {
   video.hide();
 
   faceMesh.detectStart(video, gotFace);
+
+  textAlign(CENTER);
 }
 
 function draw() {
-  // translate(-(width / 2), -(height / 2));
-  background(220);
-  fill(255, 0, 0);
-  circle(0, 0, 20);
+  ms = millis();
+
+  image(video, 0, 0, width, height);
+
+  if (faces.length > 0) {
+    face = faces[0];
+    let box = face.box;
+    if (faceDetected === 0) faceDetected = ms;
+    console.log(faceDetected);
+    push();
+    noFill();
+    strokeWeight(2);
+    rect(box.xMin + 30, box.yMin + 10, box.width, box.height);
+    pop();
+    if (ms - faceDetected > 1500) {
+      push();
+      fill(0);
+      textSize(40);
+      text("HUMAN DETECTED", width * 0.5, height * 0.1);
+      pop();
+    }
+  } else {
+    faceDetected = 0;
+  }
+}
+
+function mousePressed() {
+  console.log(face);
 }
 
 /**
@@ -34,6 +66,5 @@ function draw() {
  */
 
 function gotFace(result) {
-  face = result;
-  console.log(face);
+  faces = result;
 }
