4838e2beaf5c298f0cdff0399669440c9d39a557
Mauro Giachero <mau.giachero@gmail.com>
2025-06-30 09:47:48 -0300

    scan idea

--- sketch.js
+++ sketch.js
@@ -15,6 +15,8 @@ let faceDetected = 0;
 let video;
 2;
 let ms;
+let scanY = 0;
+let scanDir = 1;
 
 function preload() {
   faceMesh = ml5.faceMesh(options);
@@ -38,13 +40,19 @@ function draw() {
   if (faces.length > 0) {
     face = faces[0];
     let box = face.box;
+    let top = box.yMin + 10;
+    let bottom = box.yMin + box.height + 10;
+    // console.log(top, bottom);
+
+    if (scanY === 0) scanY = top;
+
     if (faceDetected === 0) faceDetected = ms;
-    console.log(faceDetected);
     push();
     noFill();
     strokeWeight(2);
     rect(box.xMin + 30, box.yMin + 10, box.width, box.height);
     pop();
+
     if (ms - faceDetected > 1500) {
       push();
       fill(0);
@@ -52,8 +60,23 @@ function draw() {
       text("HUMAN DETECTED", width * 0.5, height * 0.1);
       pop();
     }
+
+    if (ms - faceDetected > 3000) {
+      push();
+      stroke(0, 255, 0);
+      strokeWeight(2);
+      line(box.xMin + 30, scanY, box.xMin + 30 + box.width, scanY);
+      pop();
+
+      scanY += scanDir * 1.5;
+
+      if (scanY >= bottom || scanY <= top) {
+        scanDir *= -1;
+      }
+    }
   } else {
     faceDetected = 0;
+    scanY = 0;
   }
 }
 
